56061c682a0d6848256334cb5adc2880987d73ae
2SMILE2 <alwayssmile11a1@gmail.com>
2018-03-15 16:22:32 +0700

    nothing much

--- Assets/AlessiaController.cs
+++ Assets/AlessiaController.cs
@@ -2,48 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D))]
-[RequireComponent(typeof(CapsuleCollider2D))]
+[RequireComponent(typeof(CharacterController2D))]
 public class AlessiaController : MonoBehaviour {
 
-    [Tooltip("The Layers which represent gameobjects that the Character Controller can be grounded on.")]
-    public LayerMask groundedLayerMask;
-    [Tooltip("The distance down to check for ground.")]
-    public float groundedRaycastDistance = 0.1f;
     public float speed = 5f;
     public float jumpForce = 400f;
 
     public FireFlyController fireFlyController;
 
     
-
-
     public KeyCode jumpButton;
     public KeyCode shootButton;
 
+    private CharacterController2D m_CharacterController2D;
     private Vector2 m_Velocity = new Vector2();
     private Rigidbody2D m_Rigidbody2D;
     private float m_HorizontalMovement;
     private float m_VerticalForce;
     private Animator m_Animator;
-    private CapsuleCollider2D m_CapsuleCollider2D;
-    private bool isGrounded;
-    private ContactFilter2D m_ContactFilter;
-    private Vector2[] m_RaycastStartPositions = new Vector2[3];
-    private RaycastHit2D[] m_HitBuffer = new RaycastHit2D[5];
     private SpriteRenderer m_SpriteRenderer;
 
-    // Use this for initialization
+
     void Awake () {
         m_Rigidbody2D = GetComponent<Rigidbody2D>();
-        m_CapsuleCollider2D = GetComponent<CapsuleCollider2D>();
         m_SpriteRenderer = GetComponent<SpriteRenderer>();
         m_Animator = GetComponent<Animator>();
-        m_ContactFilter.layerMask = groundedLayerMask;
-        m_ContactFilter.useLayerMask = true;
-        m_ContactFilter.useTriggers = false;
-        
-        Physics2D.queriesStartInColliders = false;
+        m_CharacterController2D = GetComponent<CharacterController2D>();
     }
 
     // Update is called once per frame
@@ -53,28 +37,21 @@ public class AlessiaController : MonoBehaviour {
         m_HorizontalMovement = Input.GetAxis("Horizontal");
 
         //jump only if grounded
-        if (Input.GetKeyDown(jumpButton) && isGrounded)
+        if (Input.GetKeyDown(jumpButton) && m_CharacterController2D.IsGrounded)
         {
 
             m_VerticalForce = jumpForce;
         }
         
-
-
         //shoot
         if(Input.GetKeyDown(shootButton))
         {
             Shoot();
         }
         
-
         //set velocity 
         m_Velocity.Set(m_HorizontalMovement * speed, m_Rigidbody2D.velocity.y);
 
-        //Check grounded
-        CheckCapsuleEndCollisions();
-
-        
     }
 
 
@@ -109,7 +86,7 @@ public class AlessiaController : MonoBehaviour {
 
     private void Animate()
     {
-        m_Animator.SetBool("grounded", isGrounded);
+        m_Animator.SetBool("grounded", m_CharacterController2D.IsGrounded);
         m_Animator.SetFloat("velocityX", Mathf.Abs(m_HorizontalMovement));
     }
 
@@ -121,50 +98,6 @@ public class AlessiaController : MonoBehaviour {
         }
     }
 
-
-    /// <summary>
-    /// This updates the state of IsGrounded.  It is called automatically in Update but can be called more frequently if higher accurracy is required.
-    /// </summary>
-    public void CheckCapsuleEndCollisions()
-    {
-        Vector2 raycastDirection;
-        Vector2 raycastStart;
-        float raycastDistance;
-
-        raycastStart = m_Rigidbody2D.position + m_CapsuleCollider2D.offset;
-
-        //we add size.x/2 to this line of code because in a bit later, we subtract the  raycastStartBottomCentre to size.x/2
-        raycastDistance = m_CapsuleCollider2D.size.x * 0.5f + groundedRaycastDistance * 2f;
-
-        raycastDirection = Vector2.down;
-
-        Vector2 raycastStartBottomCentre = raycastStart + Vector2.down * (m_CapsuleCollider2D.size.y * 0.5f - m_CapsuleCollider2D.size.x * 0.5f);
-        m_RaycastStartPositions[0] = raycastStartBottomCentre + Vector2.left * m_CapsuleCollider2D.size.x * 0.5f;
-        m_RaycastStartPositions[1] = raycastStartBottomCentre;
-        m_RaycastStartPositions[2] = raycastStartBottomCentre + Vector2.right * m_CapsuleCollider2D.size.x * 0.5f;
-
-        int count = 0;
-
-        for (int i = 0; i < m_RaycastStartPositions.Length; i++)
-        {
-            //Shoot ray
-            count += Physics2D.Raycast(m_RaycastStartPositions[i], raycastDirection, m_ContactFilter, m_HitBuffer, raycastDistance);
-            Debug.DrawRay(m_RaycastStartPositions[i], raycastDirection);
-
-            if (count > 0)
-            {
-                isGrounded = true;
-                return;
-            }
-
-        }
-
-        if(count==0)
-        {
-            isGrounded = false;
-        }
-
-    }
 
 
 }
--- Assets/_Main/Scripts/CharacterController2D.cs
+++ Assets/_Main/Scripts/CharacterController2D.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 using Gamekit2D;
 
 [RequireComponent(typeof(Rigidbody2D))]
-[RequireComponent(typeof(Collider2D))]
+[RequireComponent(typeof(CapsuleCollider2D))]
 public class CharacterController2D : MonoBehaviour
 {
     [Tooltip("The Layers which represent gameobjects that the Character Controller can be grounded on.")]
@@ -12,31 +12,24 @@ public class CharacterController2D : MonoBehaviour
     public float groundedRaycastDistance = 0.1f;
 
     Rigidbody2D m_Rigidbody2D;
-    CapsuleCollider2D m_Capsule;
-    Vector2 m_PreviousPosition;
-    Vector2 m_CurrentPosition;
-    Vector2 m_NextMovement;
+    CapsuleCollider2D m_CapsuleCollider2D;
     ContactFilter2D m_ContactFilter;
     RaycastHit2D[] m_HitBuffer = new RaycastHit2D[5];
-    RaycastHit2D[] m_FoundHits = new RaycastHit2D[3];
-    Collider2D[] m_GroundColliders = new Collider2D[3];
-    Vector2[] m_RaycastPositions = new Vector2[3];
+    Collider2D[] m_GroundCollider;
+    Vector2[] m_RaycastStartPositions = new Vector2[3];
+    Vector2 m_NextMovement;
 
     public bool IsGrounded { get; protected set; }
     public bool IsCeilinged { get; protected set; }
     public Vector2 Velocity { get; protected set; }
     public Rigidbody2D Rigidbody2D { get { return m_Rigidbody2D; } }
-    public Collider2D[] GroundColliders { get { return m_GroundColliders; } }
     public ContactFilter2D ContactFilter { get { return m_ContactFilter; } }
-
+    public Collider2D[] GroundColliders { get { return m_GroundCollider; } }
 
     void Awake()
     {
         m_Rigidbody2D = GetComponent<Rigidbody2D>();
-        m_Capsule = GetComponent<CapsuleCollider2D>();
-
-        m_CurrentPosition = m_Rigidbody2D.position;
-        m_PreviousPosition = m_Rigidbody2D.position;
+        m_CapsuleCollider2D = GetComponent<CapsuleCollider2D>();
 
         m_ContactFilter.layerMask = groundedLayerMask;
         m_ContactFilter.useLayerMask = true;
@@ -45,18 +38,10 @@ public class CharacterController2D : MonoBehaviour
         Physics2D.queriesStartInColliders = false;
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        ////m_PreviousPosition = m_Rigidbody2D.position;
-        ////m_CurrentPosition = m_PreviousPosition + m_NextMovement;
-        ////Velocity = (m_CurrentPosition - m_PreviousPosition) / Time.deltaTime;
-
-        ////m_Rigidbody2D.MovePosition(m_CurrentPosition);
-
-        ////m_NextMovement = Vector2.zero;
 
         CheckCapsuleEndCollisions();
-        CheckCapsuleEndCollisions(false);
     }
 
     /// <summary>
@@ -75,160 +60,53 @@ public class CharacterController2D : MonoBehaviour
     /// <param name="position">The new position of the character in global space.</param>
     public void Teleport(Vector2 position)
     {
-        Vector2 delta = position - m_CurrentPosition;
-        m_PreviousPosition += delta;
-        m_CurrentPosition = position;
         m_Rigidbody2D.MovePosition(position);
     }
 
     /// <summary>
     /// This updates the state of IsGrounded.  It is called automatically in FixedUpdate but can be called more frequently if higher accurracy is required.
     /// </summary>
-    public void CheckCapsuleEndCollisions(bool bottom = true)
+    /// <summary>
+    /// This updates the state of IsGrounded.  It is called automatically in Update but can be called more frequently if higher accurracy is required.
+    /// </summary>
+    public void CheckCapsuleEndCollisions()
     {
         Vector2 raycastDirection;
         Vector2 raycastStart;
         float raycastDistance;
 
-        if (m_Capsule == null)
-        {
-            raycastStart = m_Rigidbody2D.position + Vector2.up;
-            raycastDistance = 1f + groundedRaycastDistance;
+        raycastStart = m_Rigidbody2D.position + m_CapsuleCollider2D.offset;
 
-            if (bottom)
-            {
-                raycastDirection = Vector2.down;
+        //we add size.x/2 to this line of code because in a bit later, we subtract the  raycastStartBottomCentre to size.x/2
+        raycastDistance = m_CapsuleCollider2D.size.x * 0.5f + groundedRaycastDistance * 2f;
 
-                m_RaycastPositions[0] = raycastStart + Vector2.left * 0.4f;
-                m_RaycastPositions[1] = raycastStart;
-                m_RaycastPositions[2] = raycastStart + Vector2.right * 0.4f;
-            }
-            else
-            {
-                raycastDirection = Vector2.up;
+        raycastDirection = Vector2.down;
 
-                m_RaycastPositions[0] = raycastStart + Vector2.left * 0.4f;
-                m_RaycastPositions[1] = raycastStart;
-                m_RaycastPositions[2] = raycastStart + Vector2.right * 0.4f;
-            }
-        }
-        else
-        {
-            raycastStart = m_Rigidbody2D.position + m_Capsule.offset;
+        Vector2 raycastStartBottomCentre = raycastStart + Vector2.down * (m_CapsuleCollider2D.size.y * 0.5f - m_CapsuleCollider2D.size.x * 0.5f);
+        m_RaycastStartPositions[0] = raycastStartBottomCentre + Vector2.left * m_CapsuleCollider2D.size.x * 0.5f;
+        m_RaycastStartPositions[1] = raycastStartBottomCentre;
+        m_RaycastStartPositions[2] = raycastStartBottomCentre + Vector2.right * m_CapsuleCollider2D.size.x * 0.5f;
 
-            //we add size.x/2 to this line of code because in a bit later, we subtract the  raycastStartBottomCentre to size.x/2
-            raycastDistance = m_Capsule.size.x * 0.5f + groundedRaycastDistance * 2f;
+        int count = 0;
 
-            if (bottom)
-            {
-                raycastDirection = Vector2.down;
-                Vector2 raycastStartBottomCentre = raycastStart + Vector2.down * (m_Capsule.size.y * 0.5f - m_Capsule.size.x * 0.5f); //why subtract size.x?
-
-                m_RaycastPositions[0] = raycastStartBottomCentre + Vector2.left * m_Capsule.size.x * 0.5f;
-                m_RaycastPositions[1] = raycastStartBottomCentre;
-                m_RaycastPositions[2] = raycastStartBottomCentre + Vector2.right * m_Capsule.size.x * 0.5f;
-            }
-            else
-            {
-                raycastDirection = Vector2.up;
-                Vector2 raycastStartTopCentre = raycastStart + Vector2.up * (m_Capsule.size.y * 0.5f - m_Capsule.size.x * 0.5f);
-
-                m_RaycastPositions[0] = raycastStartTopCentre + Vector2.left * m_Capsule.size.x * 0.5f;
-                m_RaycastPositions[1] = raycastStartTopCentre;
-                m_RaycastPositions[2] = raycastStartTopCentre + Vector2.right * m_Capsule.size.x * 0.5f;
-            }
-        }
-
-        for (int i = 0; i < m_RaycastPositions.Length; i++)
+        for (int i = 0; i < m_RaycastStartPositions.Length; i++)
         {
             //Shoot ray
-            int count = Physics2D.Raycast(m_RaycastPositions[i], raycastDirection, m_ContactFilter, m_HitBuffer, raycastDistance);
+            count += Physics2D.Raycast(m_RaycastStartPositions[i], raycastDirection, m_ContactFilter, m_HitBuffer, raycastDistance);
+            Debug.DrawRay(m_RaycastStartPositions[i], raycastDirection);
 
-            if (bottom)
+            if (count > 0)
             {
-                m_FoundHits[i] = count > 0 ? m_HitBuffer[0] : new RaycastHit2D();
-                m_GroundColliders[i] = m_FoundHits[i].collider;
-
+                IsGrounded = true;
+                return;
             }
-            else //check for ceiling
-            {
-                IsCeilinged = false;
 
-                for (int j = 0; j < m_HitBuffer.Length; j++)
-                {
-                    if (m_HitBuffer[j].collider != null)
-                    {
-                        if (!PhysicsHelper.ColliderHasPlatformEffector(m_HitBuffer[j].collider))
-                        {
-                            IsCeilinged = true;
-                        }
-                    }
-                }
-            }
         }
 
-
-        if (bottom)
+        if (count == 0)
         {
-
-            //Find groundNormal 
-            Vector2 groundNormal = Vector2.zero;
-            int hitCount = 0;
-            for (int i = 0; i < m_FoundHits.Length; i++)
-            {
-                if (m_FoundHits[i].collider != null)
-                {
-
-                    groundNormal += m_FoundHits[i].normal;
-                    hitCount++;
-                }
-            }
-
-            if (hitCount > 0)
-            {
-                groundNormal.Normalize();
-            }
-
-
-            Vector2 relativeVelocity = Velocity;
-            for (int i = 0; i < m_GroundColliders.Length; i++)
-            {
-                if (m_GroundColliders[i] == null)
-                    continue;
-
-                MovingPlatform movingPlatform;
-
-                if (PhysicsHelper.TryGetMovingPlatform(m_GroundColliders[i], out movingPlatform))
-                {
-                    relativeVelocity -= movingPlatform.Velocity / Time.deltaTime;
-                    break;
-                }
-            }
-
-            if (Mathf.Approximately(groundNormal.x, 0f) && Mathf.Approximately(groundNormal.y, 0f))
-            {
-                IsGrounded = false;
-            }
-            else
-            {
-                IsGrounded = relativeVelocity.y <= 0f;
-
-                if (m_Capsule != null)
-                {
-                    if (m_GroundColliders[1] != null)
-                    {
-                        float capsuleBottomHeight = m_Rigidbody2D.position.y + m_Capsule.offset.y - m_Capsule.size.y * 0.5f;
-                        float middleHitHeight = m_FoundHits[1].point.y;
-                        IsGrounded &= middleHitHeight < capsuleBottomHeight + groundedRaycastDistance;
-                    }
-                }
-            }
+            IsGrounded = false;
         }
 
-        //reset buffer
-        for (int i = 0; i < m_HitBuffer.Length; i++)
-        {
-            m_HitBuffer[i] = new RaycastHit2D();
-        }
     }
 }
