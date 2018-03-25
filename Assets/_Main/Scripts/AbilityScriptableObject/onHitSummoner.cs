da6cff5ac8a0133ac16cc55958c630ee513deba8
ducthong230497 <15520856@gm.uit.edu.vn>
2018-03-25 23:43:00 +0700

    add simple hit effect

---
+++ Assets/_Main/Scripts/AbilityScriptableObject/onHitSummoner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class onHitSummoner : MonoBehaviour {
+    public GameObject hitEffect;
+    [Tooltip("Just testing")]
+    public Transform particlePos;
+	void OnTriggerEnter(Collider collider)
+    {
+        //if (collider.tag.Equals("Summoner"))
+        int a = 2;
+        {
+            Debug.Log("hit: " + collider.tag);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag =="Acid")
+        {
+            Debug.Log("hit: " );
+            Instantiate(hitEffect, particlePos);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+
+}
