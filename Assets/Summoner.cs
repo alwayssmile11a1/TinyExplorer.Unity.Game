64afec16c7fe2bcd0417a933dad6b9a2357c3007
2SMILE2 <alwayssmile11a1@gmail.com>
2018-03-26 22:58:16 +0700

    just some basic summoner animation

--- Assets/Summoner.cs
+++ Assets/Summoner.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Summoner : MonoBehaviour {
 
+    public GameObject enemyToSpawn;
+
     public Transform[] spawnEnemyPositions;
     public Transform[] teleportPositions;
 
+    private Animator m_Animator;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
+    private void Awake()
+    {
+        m_Animator = GetComponent<Animator>();
+    }
+
+  
 	// Update is called once per frame
 	void Update () {
 		
@@ -21,9 +25,31 @@ public class Summoner : MonoBehaviour {
 
     public void SpawnEnemies()
     {
+        StartCoroutine(InternalSpawnEnemies());
+    }
+
+    private IEnumerator InternalSpawnEnemies()
+    {
+
+        m_Animator.SetTrigger("StartSummoning");
+
+        yield return new WaitForSeconds(2f);
+
+        if (enemyToSpawn != null || spawnEnemyPositions.Length == 0)
+        {
+
+            int randomIndex = Random.Range(0, spawnEnemyPositions.Length - 1);
+
+            GameObject cloneEnemy = Instantiate(enemyToSpawn, spawnEnemyPositions[randomIndex].position, Quaternion.identity, transform);
+
+            Destroy(cloneEnemy, 5f);
+
+
+
+        }
 
     }
-    
+
 
     public void SpawnFireballs()
     {
