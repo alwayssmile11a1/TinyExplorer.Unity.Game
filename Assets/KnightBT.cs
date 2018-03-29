9ea2d1178d0619cfbcdd3ae9b610b60edf402c23
ducthong230497 <15520856@gm.uit.edu.vn>
2018-03-29 15:23:08 +0700

    nothing

---
+++ Assets/KnightBT.cs
@@ -0,0 +1,36 @@
+﻿using BTAI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnightBT : MonoBehaviour {
+    Animator m_Animator;
+    Root m_Ai = BT.Root();
+    EnemyBehaviour m_EnemyBehaviour;
+    // Use this for initialization
+    private void OnEnable()
+    {
+        m_EnemyBehaviour = GetComponent<EnemyBehaviour>();
+        m_Animator = GetComponent<Animator>();
+
+        m_Ai.OpenBranch(
+
+            BT.If(() => { return m_EnemyBehaviour.CurrentTarget != null; }).OpenBranch(
+                BT.Call(m_EnemyBehaviour.CheckTargetStillVisible),
+                BT.Call(m_EnemyBehaviour.OrientToTarget),
+                BT.Trigger(m_Animator, "shooting"),
+                BT.Call(m_EnemyBehaviour.RememberTargetPos),
+                BT.WaitForAnimatorState(m_Animator, "KnightAttack")
+            ),
+
+            BT.If(() => { return m_EnemyBehaviour.CurrentTarget == null; }).OpenBranch(
+                BT.Call(m_EnemyBehaviour.ScanForTarget)
+            )
+        );
+    }
+
+    // Update is called once per frame
+    void Update () {
+        m_Ai.Tick();
+	}
+}
---
+++ Assets/_Main/Animations/KnightAnimation/changeAnim.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changeAnim : MonoBehaviour {
+
+    private Animator animator;
+    private bool attack;
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+        attack = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            animator.SetBool("attack", true);
+        }
+	}
+    public void EndAttackEvent()
+    {
+        //Instantiate bullet or whatsoever
+        animator.SetBool("attack", false);
+    }
+}
