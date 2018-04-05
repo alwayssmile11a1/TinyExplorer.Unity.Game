b75018133d03c5616ce7a424a5aa61298aebae57
ducthong230497 <15520856@gm.uit.edu.vn>
2018-04-05 23:02:43 +0700

    add some code to level

---
+++ Assets/_Main/Scripts/MovingGround.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class MovingGround : MonoBehaviour {
+    public Transform destination;
+    public Vector2 velocity;
+    [SerializeField]
+    private bool canMove;
+    private Rigidbody2D platformRigidbody;
+
+    private void Awake()
+    {
+        platformRigidbody = GetComponent<Rigidbody2D>();
+    }
+    // Update is called once per frame
+    void Update () {
+        if(canMove)
+        {
+            Vector2 movement = velocity * Time.deltaTime;
+            MovePlatform(movement);
+        }
+	}
+
+    void MovePlatform(Vector2 movement)
+    {
+        if (true)
+        {
+            Vector2 currentPosition = platformRigidbody.position;
+            platformRigidbody.MovePosition(currentPosition + movement);
+        }
+        //else
+        //    canMove = false;
+    }
+}
---
+++ Assets/_Main/Scripts/teleport.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class teleport : MonoBehaviour {
+    public Transform target;
+    public Transform destination;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(target.position);
+        target.position = destination.position;
+        Debug.Log(target.position);
+    }
+}
