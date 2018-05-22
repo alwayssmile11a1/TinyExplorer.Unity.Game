8a6c0b88026f6f1f6a4d027a3a59c1e0de887982
2SMILE2 <alwayssmile11a1@gmail.com>
2018-05-22 17:39:42 +0700

    nothing

---
+++ Assets/_Main/Scripts/LocalScripts/ShootingBase.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Gamekit2D;
+
+public class ShootingBase : MonoBehaviour {
+
+    public GameObject bullet;
+    public float timeBetweenShooting = 0.5f;
+    public float startDelay = 0.5f;
+    public float bulletSpeed = 5f;
+
+
+    private float m_ShootingTimer;
+    private BulletPool m_BulletPool;
+
+
+
+    // Use this for initialization
+    void Awake()
+    {
+
+        m_BulletPool = BulletPool.GetObjectPool(bullet, 2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (startDelay > 0)
+        {
+            startDelay -= Time.deltaTime;
+        }
+        else
+        {
+
+            if (m_ShootingTimer > 0)
+            {
+                m_ShootingTimer -= Time.deltaTime;
+            }
+            else
+            {
+                Shoot();
+                m_ShootingTimer = timeBetweenShooting;
+            }
+        }
+
+
+
+
+
+    }
+
+    public void Shoot()
+    {
+        BulletObject bulletObject = m_BulletPool.Pop(transform.position + transform.up * 0.8f);
+        bulletObject.transform.RotateToDirection(transform.up);
+        bulletObject.rigidbody2D.velocity = bulletSpeed * transform.up;
+    }
+}
