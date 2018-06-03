ed5a561b123da667a0368753fde98adc2b881fe0
2SMILE2 <alwayssmile11a1@gmail.com>
2018-06-03 10:40:16 +0700

    polish level

--- Assets/_Main/Scripts/LocalScripts/BoneKnight.cs
+++ Assets/_Main/Scripts/LocalScripts/BoneKnight.cs
@@ -107,4 +107,10 @@ public class BoneKnight : MonoBehaviour, IBTDebugable {
         damager.DisableDamage();
     }
 
+
+    public void PlayFootStep()
+    {
+        m_EnemyBehaviour.PlayFootStep();
+    }
+
 }
