863b9a158e362317c9cdf9286fcbd80db104ce8d
ducthong230497 <15520856@gm.uit.edu.vn>
2019-06-03 21:31:13 +0700

    nothing much

---
+++ Assets/AI_Teleport.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI_Teleport : MonoBehaviour
+{
+    public Transform destination;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("ZombieAI"))
+        {
+            collision.transform.position = destination.position;
+        }
+    }
+}
